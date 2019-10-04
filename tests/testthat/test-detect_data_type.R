f17117e3f774f9b1e408abfd799b22378d988731
Brendan <brendan.g.knapp@gmail.com>
2019-10-03 17:50:54 -0700

    more tests

--- tests/testthat/test-detect_data_type.R
+++ tests/testthat/test-detect_data_type.R
@@ -1,6 +1,7 @@
 test_that("detecting data type works", {
   temp_file <- tempfile()
   
+  # deleted status from API stream
   normal1 <- "{\"delete\":{\"status\":{\"id\":565342480917602305,\"id_str\":\"565342480917602305\",\"user_id\":1666065217,\"user_id_str\":\"1666065217\"},\"timestamp_ms\":\"1569693799718\"}}"
   write(normal1, temp_file)
   
@@ -9,7 +10,7 @@ test_that("detecting data type works", {
     0
   )
   
-
+  # "regular" status from API stream
   normal2 <- '{"id_str":"1178007813257388032"}'
   write(normal2, temp_file)
   expect_error(
@@ -17,7 +18,7 @@ test_that("detecting data type works", {
     NA
   )
 
-  
+  # Elasticsearch .jsonl tweet
   nested_doc <- '{"doc":{"id_str":"1178007813257388032"}}'
   write(nested_doc, temp_file)
   expect_equal(
@@ -25,6 +26,7 @@ test_that("detecting data type works", {
     1
   )
   
+  # Elasticsearch .jsonl non-tweet
   nested_doc <- '{"junk":[],"doc":{"kind":"test"}}'
   write(nested_doc, temp_file)
   expect_equal(
@@ -32,7 +34,16 @@ test_that("detecting data type works", {
     0
   )
   
+  # Elasticsearch .json array tweet
+  nested_doc <- '[{"_source":{"doc":{"id_str":"1178007813257388032"}}},{"_source":{"doc":{"id_str":"1178007813257388032"}}}]'
+  write(nested_doc, temp_file)
+  expect_equal(
+    nrow(read_tweets(temp_file)),
+    2
+  )
   
+
+  # Elasticsearch .json array non-tweet
   nested_doc <- '[{"junk":[],"doc":{"kind":"test"}},{"junk":[],"doc":{"kind":"test"}}]'
   write(nested_doc, temp_file)
   expect_equal(
